24b75596f03c8551629fff9a0252067bfbc33bbc
krmgst <krmgst@outlook.com>
2024-03-09 20:01:02 +0900

    Update

--- KeyExplorer/MainForm.Designer.cs
+++ KeyExplorer/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KeyExplorer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "KeyExplorer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
